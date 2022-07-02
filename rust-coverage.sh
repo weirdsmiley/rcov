9f93b64831f0021299657428a78b48e78dd77647
Manas <manas18244@iiitd.ac.in>
2022-07-02 09:37:06 +0530

    Generate html reports for coverage

--- rust-coverage.sh
+++ rust-coverage.sh
@@ -17,6 +17,15 @@ read -p "Enter parameters after: ./target/debug/$project " -r parameters
 ./target/debug/$project  $parameters
 
 $(rustc --print sysroot)/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge -sparse default.profraw -o default.profdata
+
+printf '=%.0s' {1..35} ; printf '\n'
+echo "~ Generating HTML files for coverage"
+printf '=%.0s' {1..35} ; printf '\n'
+
+llvm-cov show -instr-profile=default.profdata ./target/debug/$project ./src \
+  -format html -output-dir ./target/coverage
+firefox --new-tab ./target/coverage/index.html 2> /dev/null
+
 # without any demangler
 $(rustc --print sysroot)/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov show target/debug/$project \
     -instr-profile=default.profdata \
